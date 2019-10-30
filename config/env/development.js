92d11460edec05e3d89295cb54aad73505a5201d
Mohamed IDRISSI <med.idrissi@outlook.com>
2019-10-30 16:08:12 +0100

    chore(deps): update dependencies

--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,5 @@
+const { resolve } = require('path');
+
 module.exports = {
   db: {
     uri:
@@ -18,19 +20,18 @@ module.exports = {
   log: {
     // logging with Morgan - https://github.com/expressjs/morgan
     // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: 'dev',
+    format: process.env.LOG_FORMAT || 'combined',
     options: {
       // Stream defaults to process.stdout
       // Uncomment/comment to toggle the logging to a log on the file system
       stream: {
-        directoryPath: process.cwd(),
-        fileName: 'logs/access.log',
-        rotatingLogs: {
-          // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-          active: false, // activate to use rotating logs
-          fileName: 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
-          frequency: 'daily',
-          verbose: false,
+        directoryPath: process.env.LOG_DIR_PATH || resolve('logs'),
+        fileName: process.env.LOG_FILE || 'access.log',
+        rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
+          active: process.env.LOG_ROTATING_ACTIVE === 'true', // activate to use rotating logs
+          fileName: process.env.LOG_ROTATING_FILE || 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
+          frequency: process.env.LOG_ROTATING_FREQUENCY || 'daily',
+          verbose: process.env.LOG_ROTATING_VERBOSE === 'true',
         },
       },
     },
--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 
 module.exports = {
   port: process.env.PORT || 8443,
@@ -22,7 +23,7 @@ module.exports = {
       // Stream defaults to process.stdout
       // Uncomment/comment to toggle the logging to a log on the file system
       stream: {
-        directoryPath: process.env.LOG_DIR_PATH || process.cwd(),
+        directoryPath: process.env.LOG_DIR_PATH || resolve('logs'),
         fileName: process.env.LOG_FILE || 'access.log',
         rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
           active: process.env.LOG_ROTATING_ACTIVE === 'true', // activate to use rotating logs
