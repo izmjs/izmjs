fb4ea89d43bcd20eed23c4afd7e7ffff59082202
Mohamed IDRISSI <med.idrissi@outlook.com>
2019-11-21 22:40:05 +0100

    chore: update dependencies

--- scripts/modules-tools.sh
+++ scripts/modules-tools.sh
@@ -24,6 +24,28 @@ git-push)
   fi
   ;;
 
+git-commit)
+  git add .
+  git commit
+  git push
+  ;;
+
+git-status)
+  git status
+  ;;
+
+git-revert)
+  if ! (git diff --exit-code --quiet 2>/dev/null); then
+    git status
+    read -p "Are you sure you want to cancel the work (y/n)?" CONT
+    if [ "$CONT" = "y" ]; then
+      git checkout -- .
+    fi
+  else
+    echo "Clean workspace"
+  fi
+  ;;
+
 npm-update)
   if ! (hash ncu 2>/dev/null); then
     echo "npm-check-updates does not exist, installing it globally..."
