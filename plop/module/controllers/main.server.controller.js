2ba148695303fd4b827e8b8a266120dd3cbc3cda
Mohamed IDRISSI <med.idrissi@outlook.com>
2020-10-01 01:59:22 +0100

    chore: update dependencies

--- plop/module/controllers/main.server.controller.js
+++ plop/module/controllers/main.server.controller.js
@@ -1,5 +1,3 @@
-const { resolve } = require('path');
-
 /**
  * Check if the module "{{{lowercase name}}}" is up and running
  * @controller Check "{{{lowercase name}}}" module
@@ -7,9 +5,5 @@ const { resolve } = require('path');
  * @param {import('express').Response} res The response
  */
 exports.ok = async function ok(req, res) {
-  res.render(resolve(__dirname, '../views/main'), {
-    title: '{{{lowercase name}}}',
-    description: req.t('IS_OK'),
-    cssFiles: ['https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'],
-  });
+  res.json(true);
 };
