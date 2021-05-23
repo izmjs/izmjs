5cc6ff4d7e1accd629796c35b06608bc792e300a
AXeL-dev <contact.axel.dev@gmail.com>
2021-05-24 00:24:55 +0100

    chore(users): add some documentation

--- vendor/users/helpers/crypt.server.helper.js
+++ vendor/users/helpers/crypt.server.helper.js
@@ -1,6 +1,11 @@
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 
+/**
+ * Generates password salt
+ * 
+ * @param {String} enctype The password encryption type
+ */
 exports.generateSalt = (enctype) => {
   switch (enctype) {
     case 'bcrypt':
@@ -11,6 +16,13 @@ exports.generateSalt = (enctype) => {
   }
 };
 
+/**
+ * Hash password
+ * 
+ * @param {String} password The password string
+ * @param {String} salt The password salt
+ * @param {String} enctype The password encryption type
+ */
 exports.hash = (password, salt, enctype) => {
   if (salt && password) {
     switch (enctype) {
@@ -27,6 +39,17 @@ exports.hash = (password, salt, enctype) => {
   return password;
 };
 
+/**
+ * Verifies if password matches with the given hash
+ * NOTE: for bcrypt encryption, we don't need to provide the salt to the compare function
+ * because the salt is already included within the password hash itself
+ * so we don't even need to save the salt in that case
+ * 
+ * @param {String} password The password string
+ * @param {String} hash The password hash to compare with
+ * @param {String} salt The password salt
+ * @param {String} enctype The password encryption type
+ */
 exports.verify = (password, hash, salt, enctype) => {
   switch (enctype) {
     case 'bcrypt':
